74a40b88be0a8e8ef456708f19fc4807a0455ac3
Fitrie <mfitrie78@gmail.com>
2024-03-14 15:18:33 +0800

    feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: "*"
+  });
+
   app.use(cookieParser());
 
   await app.listen(3000);
