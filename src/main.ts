fe05153137dd6ed43a66405ef7e526440d160bc6
Fitrie <mfitrie78@gmail.com>
2024-03-14 22:47:35 +0800

    fix: fix cors origin

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: "*",
+    origin: "https://frontend-inventory-snowy.vercel.app",
     credentials: true,
   });
 
